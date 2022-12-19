4ac1f179c8b078ede2142c0af12cd9606a93fcee
andywar65 <andy.war1965@gmail.com>
2022-12-19 10:58:25 +0100

    have insertion inlines

--- admin.py
+++ admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 from leaflet.admin import LeafletGeoAdmin, LeafletGeoAdminMixin
 
-from .models import Drawing, Layer
+from .models import Drawing, Insertion, Layer
 
 
 class LayerInline(LeafletGeoAdminMixin, admin.TabularInline):
     model = Layer
     fields = ("name", "color_field", "geom")
+    extra = 0
+
+
+class InsertionInline(LeafletGeoAdminMixin, admin.TabularInline):
+    model = Insertion
+    fk_name = "block"
+    fields = ("layer", "point", "rotation", "x_scale", "y_scale")
     extra = 0
 
 
@@ -23,6 +30,9 @@ admin.site.register(Drawing, DrawingAdmin)
 
 class LayerAdmin(LeafletGeoAdmin):
     list_display = ("__str__", "drawing")
+    inlines = [
+        InsertionInline,
+    ]
 
 
 admin.site.register(Layer, LayerAdmin)
